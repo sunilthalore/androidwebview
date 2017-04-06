d6266f5a513ed3c290d56e21eb7f3355d399960a
Sunil Kumar <sunil.kumar@roadpiper.com>
2017-04-06 21:44:52 +0530

    added progressbar

--- app/src/main/java/dev/jaiguru/devtawebview/MainActivity.java
+++ app/src/main/java/dev/jaiguru/devtawebview/MainActivity.java
@@ -2,7 +2,11 @@ package dev.jaiguru.devtawebview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
         WebView webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://kika.in/");
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                if (progress == 100) {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
