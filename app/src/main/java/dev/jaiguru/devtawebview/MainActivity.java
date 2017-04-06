2f3e3b95296de02cdcd603e36c55d4bdd9c8c93a
Sunil Kumar <sunil.kumar@roadpiper.com>
2017-04-06 21:36:00 +0530

    initial commit

---
+++ app/src/main/java/dev/jaiguru/devtawebview/MainActivity.java
@@ -0,0 +1,18 @@
+package dev.jaiguru.devtawebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://kika.in/");
+    }
+}
